575fa84d3a4f3a447f8a684058b1143d0c72b6df
Jon Ellwood <jonellwood@gmail.com>
2026-08-07 11:08:07 -0400

    Respect Android system bar insets

--- android/app/src/main/java/com/jonellwood/mergestack/MainActivity.java
+++ android/app/src/main/java/com/jonellwood/mergestack/MainActivity.java
@@ -1,5 +1,38 @@
 package com.jonellwood.mergestack;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View webView = getBridge().getWebView();
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
+            Insets systemBars = windowInsets.getInsets(
+                    WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+            ViewGroup.MarginLayoutParams layoutParams =
+                    (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+
+            if (layoutParams.leftMargin != systemBars.left
+                    || layoutParams.topMargin != systemBars.top
+                    || layoutParams.rightMargin != systemBars.right
+                    || layoutParams.bottomMargin != systemBars.bottom) {
+                layoutParams.setMargins(
+                        systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                view.setLayoutParams(layoutParams);
+            }
+
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(webView);
+    }
+}
